491e5e786322932821c07a44dc8742cfde419584
nhewitt99 <nhewitt@uncc.edu>
2022-01-29 00:00:00 -0800

    Remove legacy code. Can always roll back if needed :)

--- picar/camtest.py
+++
@@ -1,9 +0,0 @@
-from picamera import PiCamera
-from time import sleep
-
-with PiCamera() as camera:
-    camera.resolution = (1024, 768)  # (640, 480)
-    camera.framerate = 24
-    camera.start_preview()
-    sleep(2)  # warmup
-    camera.capture("foo.png")
